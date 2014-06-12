d13600ec7ee41d1b5295d7fb316b278e2b8b9c53
David H Hagan <david@davidhhagan.com>
2014-06-11 22:50:29 -0400

    Successfully added 699 entries to antoine db

--- app/models.py
+++ app/models.py
@@ -4,9 +4,49 @@ from datetime import datetime
 
 class Antoine(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    cas = db.Column(db.String(20), unique = True)
+    formula = db.Column(db.String(160), unique = True)
     name = db.Column(db.String(140))
+    A = db.Column(db.String(20))
+    B = db.Column(db.String(20))
+    C = db.Column(db.String(20))
+    Tmin = db.Column(db.String(20))
+    Tmax = db.Column(db.String(20))
+    Punit = db.Column(db.String(20))
+    Tunit = db.Column(db.String(20))
+    Source = db.Column(db.String(160))
 
-    def __init__(self, cas, name):
-        self.cas = cas
+    def __init__(self, formula, name, A, B, C, Tmin, Tmax, Tunit = 'degC', Punit = 'mmHg', Source = ''):
+        self.formula = formula
         self.name = name
+        self.A = A
+        self.B = B
+        self.C = C
+        self.Tmin = Tmin
+        self.Tmax = Tmax
+        self.Tunit = Tunit
+        self.Punit = Punit
+        self.Source = Source
+
+def create():
+    import pandas as pd
+    from pandas import DataFrame as df
+
+    filename = app.config['DOWNLOADS'] + "/antoine_data.csv"
+
+    data = pd.read_csv(filename)
+
+    for index, row in data.iterrows():
+        antoine = Antoine(formula = row.Formula,
+            name = row['Compound Name'],
+            A = row.A,
+            B = row.B,
+            C = row.C,
+            Tmin = row.TMIN,
+            Tmax = row.TMAX,
+            Source = 'Knovel')
+
+        db.session.add(antoine)
+
+    db.session.commit()
+
+    return filename
--- app/views.py
+++ app/views.py
@@ -1,12 +1,35 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, abort
 from app import app, db
 from datetime import datetime
 from sqlalchemy import desc
 import os.path
+from models import Antoine
 
 @app.route('/')
 @app.route('/index')
 def index():
     meta = 'Thermodynamic chemistry databases that are easy to search, read, and use!'
     return render_template('index.html',
+        meta_description = meta)
+
+@app.route('/db/<db_name>/')
+def db(db_name = None):
+    meta = "Database of thermodynamic properties."
+    if db_name is None:
+        flash('Sorry. That database does not exist yet.', 'error')
+        return redirect( url_for('index') )
+
+    # retrieve the database by db_name
+    if db_name == 'Antoine':
+        try:
+            db = Antoine.query.order_by('formula').all()
+        except:
+            flash("Error getting information from database", 'error')
+            return redirect( url_for('index') )
+
+    else:
+        return abort(404)
+
+    return render_template('db.html',
+        db = db,
         meta_description = meta)
